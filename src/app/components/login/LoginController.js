b1ccc0b37d24f0c1467642d12890a4938dfb1567
LaToya <llrich30032@yahoo.com>
2015-07-19 01:37:23 -0400

    added controller

---
+++ src/app/components/login/LoginController.js
@@ -0,0 +1,7 @@
+/* Authored by LaToya Richard */
+(function () {
+    function LoginController($scope) {
+        $scope.name = 'LaToya';
+    }
+    angular.module('payTracker').controller('LoginController', LoginController);
+}());
